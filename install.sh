893432e603eb733a3202ecfbd9b8b8dee51aaa34
Elmar Fasel <Elmar.Fasel@springer.com>
2020-07-21 15:53:48 +0200

    Install AnyConnect components without user interaction

--- install.sh
+++ install.sh
@@ -14,10 +14,11 @@ tar xzf /vagrant/packages/anyconnect.tar.gz -C ~/anyconnect --strip-components=1
 bash -c "sudo mkdir -p /usr/share/icons/hicolor/{48x48,64x64,96x96,128x128,256x256}/apps /usr/share/desktop-directories /usr/share/applications/"
 
 cd ~/anyconnect/vpn
-yes | ./vpn_install.sh 2 > /dev/null || true
+rm license.txt
+./vpn_install.sh
 
 cd ~/anyconnect/posture
-yes | ./posture_install.sh 2 > /dev/null || true
+./posture_install.sh --no-license --no-prompt
 
 mv /opt/.cisco/certificates/ca /opt/.cisco/certificates/ca.orig
 ln -sf /etc/ssl/certs/ /opt/.cisco/certificates/ca
