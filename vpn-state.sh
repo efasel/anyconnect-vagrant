7867097a37e8f16dc7a74d061b67097a6b6657c5
Elmar Fasel <Elmar.Fasel@springer.com>
2020-07-23 14:19:23 +0200

    Align sshuttle status with VPN status

--- vpn-state.sh
+++ vpn-state.sh
@@ -9,11 +9,20 @@ fi
 
 source .vmname
 echo "VM:    $VMNAME"
-echo "VPN:   $(ssh -F .ssh_config_vagrant "$VMNAME" "/opt/cisco/anyconnect/bin/vpn state | grep -m 1 -o 'state:.*' | sed -e 's/state: //'")"
+VPNSTATE=$(ssh -F .ssh_config_vagrant "$VMNAME" "/opt/cisco/anyconnect/bin/vpn state | grep -m 1 -o 'state:.*' | sed -e 's/state: //'")
+echo "VPN:   $VPNSTATE"
 
 SSHUTTLEPID=$(pgrep sshuttle || true)
 if [ -n "$SSHUTTLEPID" ]; then
   echo "proxy: sshuttle is running, PID: $SSHUTTLEPID"
+  if [ "$VPNSTATE" = "Disconnected" ]; then
+    echo "Stopping running sshuttle instance with PID $SSHUTTLEPID"
+    kill "$SSHUTTLEPID"
+  fi
 else
   echo "proxy: sshuttle is not running"
+  if [ "$VPNSTATE" = "Connected" ]; then
+    echo "starting sshuttle"
+    ./forward.sh
+  fi
 fi
