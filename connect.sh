64a8181eb90dfe51ce733283feec35ccb8743912
Elmar Fasel <Elmar.Fasel@springer.com>
2020-08-05 10:29:25 +0200

    Remove verify option
    This is not needed if you connect using a host instead of a profile name.

--- connect.sh
+++ connect.sh
@@ -27,24 +27,6 @@ vpn_down() {
     "$_CISCO_VPN" disconnect
 }
 
-vpn_verify() {
-    local VPN_PROFILE="$1"
-    local VPN_USERNAME="$2"
-
-    local RESULT=0
-
-    echo -n "Checking profile $VPN_PROFILE exists..."
-    if [[ ! -f "/opt/cisco/anyconnect/profile/$VPN_PROFILE.xml" ]]; then
-        echo -e "${_RED}FAILED${_NC}"
-        echo "Profile $VPN_PROFILE was not found - please connect once via '$_CISCO_VPN' to download this"
-        RESULT=1
-    else 
-        echo -e "${_GREEN}OK${_NC}"
-    fi
-
-    return $RESULT
-}
-
 verify_prerequisites() {
     if [[ ! -f "$_CISCO_VPN" ]]; then
         echo "Cannot find AnyConnect at $_CISCO_VPN – please ensure you've installed it."
@@ -87,10 +69,6 @@ main() {
         ;;
     down)
         vpn_down
-        ;;
-    verify)
-        verify_user_pass "$VPN_PROFILE" "$VPN_USERNAME" "$VPN_PASSWORD"
-        vpn_verify "$VPN_PROFILE" "$VPN_USERNAME"
         ;;
     *)
         echo "Usage: $0 <up|down|verify>"
