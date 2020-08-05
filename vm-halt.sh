db5b378c16ae36c06cfe0232442546ede4aa75e7
Elmar Fasel <Elmar.Fasel@springer.com>
2020-08-05 10:30:07 +0200

    Notify when VM is down

--- vm-halt.sh
+++ vm-halt.sh
@@ -10,11 +10,16 @@ fi
 
 VM_STATUS=$(vagrant status --machine-readable | grep ",state,"  | awk -F, '{print $4}')
 if [ "$VM_STATUS" = "running" ]; then
+  source .vmname
   echo "halting VM"
   vagrant halt
 
   rm .vmname
   rm .ssh_config_vagrant
+
+  if [ $(which notify-send > /dev/null; echo $?) -eq 0 ]; then
+    notify-send -t 3000 -i call-stop "VPN status" "VPN VM $VMNAME is down"
+  fi
 else
   echo "VM not running"
 fi
